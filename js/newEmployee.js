5b0d57a9f0a7da688677caba2996c37c29dc687c
lal28 <lucasprolopes@gmail.com>
2025-03-29 12:35:37 -0300

    implementado local storage

--- js/newEmployee.js
+++ js/newEmployee.js
@@ -1,34 +1,45 @@
 //inicio tabs
 let abaAtual = 0;
-const totalAbas = document.querySelectorAll('.tab-content').length;
+const totalAbas = document.querySelectorAll(".tab-content").length;
 
 function showTab(index) {
     abaAtual = index;
-    document.querySelectorAll('.tab').forEach((tab, i) => {
-        tab.classList.toggle('active', i === index);
-        document.getElementById('tab-' + i).classList.toggle('active', i === index);
+    document.querySelectorAll(".tab").forEach((tab, i) => {
+        tab.classList.toggle("active", i === index);
+        document.getElementById("tab-" + i).classList.toggle("active", i === index);
     });
 
     // Configura visibilidade dos botões
-    document.getElementById('btnAnterior').disabled = abaAtual === 0;
-    document.getElementById('btnProximo').style.display = (abaAtual === totalAbas - 1) ? 'none' : 'inline-block';
-    document.getElementById('btnSalvar').style.display = (abaAtual === totalAbas - 1) ? 'inline-block' : 'none';
+    document.getElementById("btnAnterior").disabled = abaAtual === 0;
+    document.getElementById("btnProximo").style.display =
+        abaAtual === totalAbas - 1 ? "none" : "inline-block";
+    document.getElementById("btnSalvar").style.display =
+        abaAtual === totalAbas - 1 ? "inline-block" : "none";
 }
 
 function mudarAba(direcao) {
-    if ((abaAtual + direcao) >= 0 && (abaAtual + direcao) < totalAbas) {
+    if (abaAtual + direcao >= 0 && abaAtual + direcao < totalAbas) {
         showTab(abaAtual + direcao);
     }
 }
 
-document.getElementById('cadastroForm').addEventListener('submit', function(event) {
-    event.preventDefault(); // Impede recarga da página
-    const formData = new FormData(this);
-    const dados = {};
-    formData.forEach((value, key) => dados[key] = value);
-    console.log("Dados do Formulário:", dados);
-    alert("Cadastro enviado com sucesso!");
-});
+document
+    .getElementById("cadastroForm")
+    .addEventListener("submit", function (event) {
+        event.preventDefault(); // Impede recarga da página
+        const formData = new FormData(this);
+        const dados = {};
+        formData.forEach((value, key) => (dados[key] = value));
+        //salvar no local storage
+        localStorage.setItem("cadastro", JSON.stringify(dados));
+        console.log("Dados do Formulário:", dados);
+        alert("Cadastro enviado com sucesso!");
+    });
 //fim tabs
+//limpar local storage
+function limpar(){
+    localStorage.clear();
+}
+
 
 //validações
